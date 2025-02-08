56fd067474093311a402210d4521acc1138411b8
Luis Coronel <lufecoro@outlook.com>
2025-02-08 08:26:15 -0500

    Ajuste css tabla detalle moras
    Ajuste css tabla detalle moras

--- application/views/abono.php
+++ application/views/abono.php
@@ -410,7 +410,7 @@ date_default_timezone_set('America/Bogota');
 							<td colspan="3">
 								<div class="collapse" id="collapseExample" style="margin: 5% 0%;">
 									<div class="card card-body">
-										<table>
+										<table class="table" style="font-size: x-small; line-height: normal;">
 											<thead>
 												<th>N° Cuota</th>
 												<th>Fecha</th>
